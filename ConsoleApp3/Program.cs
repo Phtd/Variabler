88836c5f18202a0561275cdb4eeed40105578300
Patrick Heesch (PATR371F) <patr371f@zbc.dk>
2021-01-26 10:06:13 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static int a = 16;
+        static double b = 5.1;
+        static void Main(string[] args)
+        {
+
+            int c = a + b; // This wont be executed, because we cant convert a double into a int. int can only contain whole numbers.
+
+            double d = a + b; // here it can be executed since a double can handle both whole numbers and numbers with a comma in them
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.ReadLine();
+        }
+    }
+}
